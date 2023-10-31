65a516b5c224862c7f005271e28721a8c8e4feed
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2023-10-31 19:30:40 -0400

    changed name

--- Python-Platformer-main/Python-Platformer-main/tutorial.py
+++ Python-Platformer-main/Python-Platformer-main/tutorial.py
@@ -6,7 +6,7 @@ from os import listdir
 from os.path import isfile, join
 pygame.init()
 
-pygame.display.set_caption("Platformer")
+pygame.display.set_caption("Lydia's Piano Studio Platformer")
 
 BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
@@ -14,6 +14,48 @@ FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // wodth + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i * width, j * height]
+            tiles.append(pos)
+
+    return tiles, image       
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update
+
+
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Purple.png")
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+
+    pygame.quit()
+    quit()        
+
+if __name__ == "__main__":
+    main(window)
+
+
+
+
 
 
 def flip(sprites):
